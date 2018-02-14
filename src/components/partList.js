08451caa434d0f275a3248347338296e4fad4a08
Kyle Andrews <kylemandrews@gmail.com>
2018-02-14 15:04:13 -0500

    Component Skeleton

---
+++ src/components/partList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import parts from '../partData'
+import PartShow from './partShow'
+
+class PartList extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <PartShow/>
+      </div>
+    )
+  }
+}
+
+export default PartList
