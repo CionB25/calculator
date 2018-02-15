bb47ee0a1fb9e5082662a3897406bfb7c02e2dd0
Kyle Andrews <kylemandrews@gmail.com>
2018-02-15 17:16:52 -0500

    Set Sizes

--- src/components/dataForm.js
+++ src/components/dataForm.js
@@ -1,24 +1,47 @@
 import React from 'react'
-import {Form, Select} from 'semantic-ui-react'
+import {Form} from 'semantic-ui-react'
 
-const DataForm = ({partInfo}) => {
-const options = partInfo
-const part = partInfo
-console.log(partInfo)
-// debugger
+const DataForm = ({part,sizes,handleAttachment}) => {
 
-
-
-  let forField;
-if (partInfo.length > 0) {
-  forField = <Form.Select options={options} placeholder="Select Attachment"/>;
+let attachmentField
+if (part.length > 0) {
+  // console.log(part[0].attachments)
+  const options = part[0].attachments.map(part => {
+    return {key: part.name, text: part.name, value: part.name}
+  })
+  console.log(part[0])
+  attachmentField = <Form.Select options={options} placeholder="Select Attachment" onChange={handleAttachment} />;
 } else {
-  forField= <Form loading  placeholder="loading..."/>;
+  attachmentField = <Form loading  placeholder="loading..."/>;
 }
+
+let sizeField
+if (sizes.length > 0) {
+  const arrOfKeys = sizes.map(size => {
+    return Object.keys(size)
+  })
+
+  const keys = arrOfKeys.join().split(",")
+  const options = keys.map(item => {
+    return {key: item, text: item, value: item}
+  })
+
+console.log(options)
+  sizeField = <Form.Select options={options} placeholder="Select Size"  />
+} else {
+  sizeField = <Form loading  placeholder="loading..."/>;
+}
+
 
   return (
     <div>
-        {forField}
+      <Form.Group>
+      {attachmentField}
+      {sizeField}
+        <Form.Field placeholder="loading...">
+          <input placeholder='First Name' />
+        </Form.Field>
+      </Form.Group>
     </div>
   )
 }
--- src/containers/calculatorContainer.js
+++ src/containers/calculatorContainer.js
@@ -10,6 +10,7 @@ class CalculatorContainer extends React.Component {
 
     this.state = {
       parts: parts,
+      currentPart: {},
       partInfo: [],
       attachments: [{
         description: "",
@@ -21,45 +22,65 @@ class CalculatorContainer extends React.Component {
   }
 
   hashItOut = (e) => {
-    const newArray = parts.filter(item => {
-        if (item.description === String(e.target.innerHTML)) {
-          return item
-        }
-      })
+    // const newArray = parts.filter(item => {
+    //     if (item.description === String(e.target.innerHTML)) {
+    //       return item
+    //     }
+    //   })
+    // const  newNew = newArray[0].attachments.map(item => {
+    //     // return {key: item.name, items: item}
+    //     return {key: item.name, text: item.name, value: item.name}
+    //   })
+    //
+    // const current = parts.filter(part => {
+    //   return part.description === String(e.target.innerHTML)
+    // })
+    // console.log(current)
+    //
+    // this.setState({
+    //   partInfo: newNew,
+    //   currentPart: current
+    // })
 
-      // console.log(newArray[0].attachments)
-    const  newNew = newArray[0].attachments.map(item => {
-        return {key: item.name, text: item.name, value: item.name}
-      })
-      // console.log(newNew)
-    // return {key: item.name, text: item.name, value: item.name}
+    const part = parts.filter(part => {
+          return part.description === String(e.target.innerHTML)
+        })
+
     this.setState({
-      partInfo: newNew
+      currentPart: part
     })
   }
 
   handlePart = (e) => {
     e.preventDefault()
     this.hashItOut(e)
-    // console.log(this.filterArray(parts,e))
-    // console.log(e.target.innerHTML)
-    // const pI = parts.filter(item => {
-    //   return item.description === String(e.target.innerHTML)
-    // })
-
-    // this.setState({
-    //   partInfo: parts.filter(item => {
-    //     return item.description === String(e.target.innerHTML)
-    //   })
-    // })
+    // console.log(this.state.currentPart);
   }
+
+  handleAttachment = (e) => {
+    e.preventDefault()
+    const partList = this.state.currentPart[0].attachments
+    const selectedPart = partList.filter(part => {
+      return part.name === String(e.target.innerText)
+    })
+    const sizes = selectedPart[0].sizes
+    console.log(sizes);
+    console.log(sizes[0])
+    this.setState({
+      sizes: sizes
+    })
+    // console.log(e.target.innerText);
+  }
+
+
+
 
   render() {
     // console.log(this.state.partInfo)
     return (
       <div>
         <PartList parts={this.state.parts} handlePart={this.handlePart}/>
-        <DataForm partInfo={this.state.partInfo}/>
+        <DataForm part={this.state.currentPart} sizes={this.state.sizes} handleAttachment={this.handleAttachment}/>
       </div>
     )
   }
