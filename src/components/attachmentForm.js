8e26f8ebe0b4a39476b5b54361bd7f412f47f4b9
Kyle Andrews <kylemandrews@gmail.com>
2018-03-14 12:56:17 -0400

    Get Cart Inputs Working/Clean Up Code

--- src/components/attachmentForm.js
+++ src/components/attachmentForm.js
@@ -2,16 +2,15 @@ import React from 'react'
 import {Header, Dropdown} from 'semantic-ui-react'
 
 const AttachmentForm = ({part,handleAttachment}) => {
-console.log(part);
+
   let attachmentField
   if (part.length > 0) {
-console.log(part)
     const options = part.map(part => {
       return {key: part.name, text: part.name, value: part.name}
     })
 
     attachmentField = <Dropdown fluid search selection options={options} placeholder="Select Attachment" onChange={handleAttachment}/>
-    // attachmentField = <Form.Select options={options}  />;
+
   } else {
     attachmentField = <Dropdown placeholder="loading..."/>;
   }
--- src/components/deleteButton.js
+++ src/components/deleteButton.js
@@ -3,32 +3,8 @@ import {Button} from 'semantic-ui-react'
 
 const DeleteButton = (props) => {
 
-const cart = props.cart
 const deleteItem  = props.deleteItem
 const itemID = props.item
-
-  const sayHi = (e) => {
-    e.preventDefault()
-    console.log('hi');
-    // console.log(carts);
-    // console.log(item);
-    // console.log(e.target.value);
-console.log(itemID);
-    //   const gucci = cart.filter(eh => {
-    //       return eh.name !== itemID
-    //   })
-    //   deleteItem(gucci)
-      console.log(e.target.value);
-    }
-    // // console.log(props);
-    console.log(deleteItem);
-    // console.log(gucci);
-
-    // console.log(deleteItem(gucci));
-
-
-
-// cart.size.id
 
   return (
     <Button  content="delete" value={itemID} onClick={deleteItem}/>
